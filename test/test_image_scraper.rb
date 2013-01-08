622c00abced6290d9a4461275ed986e94a277629
Matt McMahand <mmcmahand@gmail.com>
2013-01-07 20:10:41 -0500

    add travis-ci integration

--- test/test_image_scraper.rb
+++ test/test_image_scraper.rb
@@ -8,11 +8,14 @@ require 'helper'
 class TestImageScraper < Test::Unit::TestCase
   should "return list of all image urls on a web page with absolute paths" do
     images = ["http://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/SIPI_Jelly_Beans_4.1.07.tiff/lossy-page1-220px-SIPI_Jelly_Beans_4.1.07.tiff.jpg",
-              "http://bits.wikimedia.org/static-1.21wmf4/skins/common/images/magnify-clip.png",
-              "http://bits.wikimedia.org/static-1.21wmf4/skins/vector/images/search-ltr.png?303-4",
+              "http://bits.wikimedia.org/static-1.21wmf5/skins/common/images/magnify-clip.png",
+              "http://bits.wikimedia.org/static-1.21wmf5/skins/vector/images/search-ltr.png?303-4",
               "http://bits.wikimedia.org/images/wikimedia-button.png",
-              "http://bits.wikimedia.org/static-1.21wmf4/skins/common/images/poweredby_mediawiki_88x31.png"]
+              "http://bits.wikimedia.org/static-1.21wmf5/skins/common/images/poweredby_mediawiki_88x31.png"]
     scraper = ImageScraper::Client.new("http://en.wikipedia.org/wiki/Standard_test_image",:include_css_images=>false)
+
+    assert_equal images.size, scraper.image_urls.size
+
     assert_equal images, scraper.image_urls
   end
 
@@ -21,16 +24,19 @@ class TestImageScraper < Test::Unit::TestCase
     html = IO.read(File.dirname(__FILE__)+"/resources/extra_whitespace.html")
     client.doc = Nokogiri::HTML(html)
     images = ["http://g-ecx.images-amazon.com/images/G/01/SIMON/IsaacsonWalter._V164348457_.jpg","http://g-ecx.images-amazon.com/images/G/01/SIMON/IsaacsonWalter.jpg"]
+    
     assert_equal images, client.image_urls
   end
 
   should "return list of all image urls on a web page with relative paths" do
     images = ["//upload.wikimedia.org/wikipedia/commons/thumb/b/b6/SIPI_Jelly_Beans_4.1.07.tiff/lossy-page1-220px-SIPI_Jelly_Beans_4.1.07.tiff.jpg",
-              "//bits.wikimedia.org/static-1.21wmf4/skins/common/images/magnify-clip.png",
-              "//bits.wikimedia.org/static-1.21wmf4/skins/vector/images/search-ltr.png?303-4",
+              "//bits.wikimedia.org/static-1.21wmf5/skins/common/images/magnify-clip.png",
+              "//bits.wikimedia.org/static-1.21wmf5/skins/vector/images/search-ltr.png?303-4",
               "//bits.wikimedia.org/images/wikimedia-button.png",
-              "//bits.wikimedia.org/static-1.21wmf4/skins/common/images/poweredby_mediawiki_88x31.png"]
+              "//bits.wikimedia.org/static-1.21wmf5/skins/common/images/poweredby_mediawiki_88x31.png"]
     scraper = ImageScraper::Client.new("http://en.wikipedia.org/wiki/Standard_test_image",:convert_to_absolute_url=>false,:include_css_images=>false)
+    
+    assert_equal images.size, scraper.image_urls.size
     assert_equal images, scraper.image_urls
   end
 
@@ -38,6 +44,7 @@ class TestImageScraper < Test::Unit::TestCase
     scraper = ImageScraper::Client.new ""
     scraper.doc = Nokogiri::HTML(IO.read(File.dirname(__FILE__)+"/resources/stylesheet_test.html"))
     scraper.url = "http://test.com"
+    
     assert_equal ["http://test.com/css/master.css", "http://test.com/css/master2.css"], scraper.stylesheets
   end
 
