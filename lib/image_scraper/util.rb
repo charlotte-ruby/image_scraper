a100b7a36f121c04d3a33ef99a66aaea0da47c41
Matt <invalidusrname@users.noreply.github.com>
2020-06-07 16:22:39 -0400

    Add rubocop to workflow (#29)
    * run rubocop everywhere
    * add rubocop to workflow

--- lib/image_scraper/util.rb
+++ lib/image_scraper/util.rb
@@ -14,7 +14,10 @@ module ImageScraper
       #          'http://example.com/style.css
       #          but should get:
       #          'http://example.com/about/style.css
-      URI.parse(url).merge(URI.parse asset.to_s).to_s rescue nil
+
+      URI.parse(url).merge(URI.parse(asset.to_s)).to_s
+    rescue StandardError
+      nil
     end
 
     def self.domain(url)
@@ -32,21 +35,23 @@ module ImageScraper
     end
 
     def self.strip_backslashes(image_url)
-      image_url.gsub("\\",'')
+      image_url.gsub('\\', '')
     end
-    
+
     def self.strip_quotes(image_url)
       image_url.gsub("'", '').gsub('"', '')
     end
 
     def self.chomp(image_url)
-      image_url.gsub(/\s/,'')
+      image_url.gsub(/\s/, '')
     end
 
     def self.cleanup_url(image_url)
       ImageScraper::Util.chomp(
         ImageScraper::Util.strip_quotes(
-          ImageScraper::Util.strip_backslashes(image_url || '')))
+          ImageScraper::Util.strip_backslashes(image_url || '')
+        )
+      )
     end
   end
 end
